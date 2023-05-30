2d13b3eedbce10ac06142107d946bc9445d5af6a
Xinming Shi <55978851+embeddedsky@users.noreply.github.com>
2023-05-31 00:27:37 +0800

    Update main.py
    Updating some Chinese into English.

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from RC_all import *
 import sys
 if __name__=="__main__":
     # argv:
-    # 1.RC方法名：RC_CRJ ,RC_RND,RC_CND,RC_DCND
-    # 2.是否自适应: "1"--自适应，"0"--不自适应
-    # 3.优化问题: "Narma","Three_Out"
-    # 4.模型：CHEN ,HP
+    # 1.RC topology：RC_CRJ ,RC_RND,RC_CND,RC_DCND
+    # 2.If adaptive?: "1"--adaptive，"0"--not adaptive
+    # 3.Problems: "Narma","Three_Out"
+    # 4.Memristor Model：CHEN ,HP
     RC_Method,Is_Adapt,Problem,Model=sys.argv[1:]
     rc=eval(RC_Method)(Is_Adapt)
     rc.evolve(500)
